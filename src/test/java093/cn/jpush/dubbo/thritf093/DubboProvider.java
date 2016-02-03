1465460a9f1b6573e9970fccdb65cf73832819ba
rocyuan <yuanzp@jpush.cn>
2016-02-03 17:10:00 +0800

    ## 1.0.0
    * 由原来的dubbo-thrift-rpc发展而来
    * 里面保留了网上一段thrift2+netty的扩展
    * 自己扩展了thrift093,还是个不完全品,但完全可以投入使用
    
    ## 1.0.1
    * **增加 container扩展,让所有的jpush 公司的 dubbo项目都可以以同一份脚本优雅的启动dubbo项目,避免shutdown的异常和端口占用问题**
    * 不再依赖 libthrift

--- src/test/java093/cn/jpush/dubbo/thritf093/DubboProvider.java
+++ src/test/java093/cn/jpush/dubbo/thritf093/DubboProvider.java
@@ -1,20 +1,3 @@
-/**
- * The MIT License (MIT)
- *
- * Copyright (c) [2015] [rocyuan at jpush]
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE PROCTED OFFER SOME PLUGINS OF DUBBO FOR JPUSH, WITH IT WE WILL DO SOA EASIRY.
- */
 package cn.jpush.dubbo.thritf093;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
